4483777ed7c35e3d3a950f5bee92a2a08a1ca78a
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-05-01 22:55:49 +0100

    Section 6 - Asynchronous JavaScript, Ajax & Fetch API - 6.15. Custom HTTP Library (Fetch With Async Await) Part 4.

---
+++ Section 06 - Asynchronous JavaScript, Ajax & Fetch API/6.15 - Custom HTTP Library (Fetch With Async Await) Part 4/js_sandbox_6.15/easyhttp.js
@@ -0,0 +1,56 @@
+/* 
+----------------------------- 
+Custom HTTP Library (Fetch With Async Await) Part 4
+----------------------------- 
+*/
+
+
+//-----------------------------
+// EasyHTTP CLASS
+//-----------------------------
+class EasyHTTP {
+   // Make an HTTP GET Request 
+   async get(url) {
+      const response = await fetch(url);
+      const resData = await response.json();
+      return resData;
+   }
+
+   // Make an HTTP POST Request
+   async post(url, data) {
+      const response = await fetch(url, {
+         method: 'POST',
+         headers: {
+            'Content-type': 'application/json'
+         },
+         body: JSON.stringify(data)
+      });
+      const resData = await response.json();
+      return resData;
+   }
+
+   // Make an HTTP PUT Request
+   async put(url, data) {
+      const response = await fetch(url, {
+         method: 'PUT',
+         headers: {
+           'Content-type': 'application/json'
+         },
+         body: JSON.stringify(data)
+      });
+      const resData = await response.json();
+      return resData;
+   }
+
+   // Make an HTTP DELETE Request
+   async delete(url) {
+      const response = await fetch(url, {
+         method: 'DELETE',
+         headers: {
+            'Content-type': 'application/json'
+         }
+      });
+      const resData = await 'Resource Deleted...';
+      return resData;
+   }
+}
