c5b6063090d4f54fcd3a19db988666fdf10d7689
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-05-05 16:14:26 +0100

    Section 7 - API Project - WeatherJS - 7.9. Save Location to Local Storage.

---
+++ Section 07 - API Projects/WeatherJS/7.9 - Save Location to Local Storage/app.js
@@ -0,0 +1,54 @@
+//-------------------------
+// INIT NEW CLASS OBJECT:
+//-------------------------
+// Init storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
+
+// Init weather object
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+
+// Init UI
+const ui = new UI();
+
+
+//-------------------------
+// ADD EVENT LISTENER ON LOAD:
+//-------------------------
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather);
+
+
+//-------------------------
+// ADD EVENT LISTENER CHANGE LOCATION BUTTON:
+//-------------------------
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+   const city = document.getElementById('city').value;
+   const state = document.getElementById('state').value;
+
+   // Change location
+   weather.changeLocation(city, state);
+
+   // Set location in Local Storage
+   storage.setLocationData(city, state);
+
+   // Get and display weather
+   getWeather();
+
+   // Close modal
+   $('#locModal').modal('hide');
+});
+
+
+//-------------------------
+// getWeather FUNCTION:
+//-------------------------
+function getWeather(){
+weather.getWeather()
+   .then(results => {
+      ui.paint(results);
+   })
+   .catch(err => console.log(err));
+}
